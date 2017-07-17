1a234ee14010780add77467d2c7cb5175f002d99
Jimmy <Jimmy@192.168.1.107>
2017-07-17 10:32:52 +0800

    20170717_TEST

--- test/DB_TEST.java
+++ test/DB_TEST.java
@@ -1,9 +1,12 @@
+
+import model.DB;
+import model.DataTable;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Jimmy
@@ -11,6 +14,8 @@
 public class DB_TEST {
 
     public static void main(String[] args) {
-
+        DataTable dt = DB.getDataTable("SELECT * FROM lottodb.game_setting_num1");
+        String test = dt.getColume(0, "ball_1");
+        System.out.println(test);
     }
 }
